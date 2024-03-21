c7553b551bedbdbecea35da24f1b822bfc4dc9a1
Vincevdb1 <vincent.vandebosch@icloud.com>
2024-03-21 10:33:54 +0000

    feat: Add vscode launch for cassandra-java and change how the balloonController interacts with repo

--- cassandra-java/src/main/java/com/example/cassandrajava/controllers/BalloonController.java
+++ cassandra-java/src/main/java/com/example/cassandrajava/controllers/BalloonController.java
@@ -3,12 +3,15 @@ package com.example.cassandrajava.controllers;
 import com.example.cassandrajava.domain.Balloon;
 import com.example.cassandrajava.repositories.BalloonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/balloons")
@@ -22,9 +25,10 @@ public class BalloonController {
     }
 
     @PostMapping
-    public void addDummyBalloon() {
-        var balloon = Balloon.builder().name("balloon1").build();
+    public ResponseEntity<?> addDummyBalloon() {
+        var balloon = Balloon.builder().id(UUID.randomUUID()).name("balloon1").build();
 
         balloonRepository.save(balloon);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
